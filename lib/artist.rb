02b907e206adfc85eacf3d44b0856949eb306012
Amanda <ajnikrant@gmail.com>
2021-02-26 16:37:22 -0500

    commit tree 1b49c8cd32117b3d7afd0b7e977428e59975bbf2

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,32 @@
 
+class Artist
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        Song.new(name,self,genre)
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def genres
+        songs.map{|song| song.genre}
+    end
+
+end
+
+# def new_meal(customer, total, tip=0)
+#     Meal.new(self, customer, total, tip)
+#   end
